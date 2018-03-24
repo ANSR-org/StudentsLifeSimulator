fb166867c04ca3b16a2ad840b1094f7cb5ae2800
A4B5TQ <ivanov.ivan_@abv.bg>
2018-03-24 13:07:47 +0200

    Initialize database configuration

--- src/main/java/bg/ansr/simulator/studentsimulatorcore/StudentSimulatorCoreApplication.java
+++ src/main/java/bg/ansr/simulator/studentsimulatorcore/StudentSimulatorCoreApplication.java
@@ -2,8 +2,10 @@ package bg.ansr.simulator.studentsimulatorcore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource(value = {"classpath:app.properties", "classpath:local.properties"})
 public class StudentSimulatorCoreApplication {
 
 	public static void main(String[] args) {
