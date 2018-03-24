73f7d3865f4526c7c5fafa3ce21d973821571271
RoYaLBG <royal4o@gmail.com>
2018-03-24 12:45:36 +0200

    initial project structure

---
+++ src/main/java/bg/ansr/simulator/studentsimulatorcore/StudentSimulatorCoreApplication.java
@@ -0,0 +1,12 @@
+package bg.ansr.simulator.studentsimulatorcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentSimulatorCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentSimulatorCoreApplication.class, args);
+	}
+}
---
+++ src/test/java/bg/ansr/simulator/studentsimulatorcore/StudentSimulatorCoreApplicationTests.java
@@ -0,0 +1,16 @@
+package bg.ansr.simulator.studentsimulatorcore;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StudentSimulatorCoreApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
